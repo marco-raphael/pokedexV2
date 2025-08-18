a0affef5b6c2bd8fa6af17c534cbcc209884fa1a
marco-raphael <marco-raphael@hotmail.com>
2025-08-18 14:58:22 -0300

    organizacao das cores e reorganizacao das pastas

--- pokedex-v2/src/pages/DetailPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const DetailPage = () => {
-return <h1>DetailPage</h1>
-}
-export default DetailPage
--- pokedex-v2/src/pages/PokedexPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const PokedexPage = () => {
-return <h1>PokedexPage</h1>
-}
-export default PokedexPage
